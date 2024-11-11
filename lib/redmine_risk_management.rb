8117e9af861c2c7ad03e3e7c35e22b093ea17340
arean82 <38061300+Arean82@users.noreply.github.com>
2024-11-11 14:02:25 +0530

    Delete lib directory

--- lib/redmine_risk_management.rb
+++
@@ -1,5 +0,0 @@
-ActionDispatch::Reloader.to_prepare do
-  require_dependency 'redmine_risk_management/hooks/views_issues_hook'
-  require_dependency 'redmine_risk_management/patches/issue_patch'
-  require_dependency 'redmine_risk_management/patches/project_patch'
-end
