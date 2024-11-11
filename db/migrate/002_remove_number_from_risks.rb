2eb78e1822a6022ac61657c0ff627d61bfa5c8f2
arean82 <38061300+Arean82@users.noreply.github.com>
2024-11-11 14:02:10 +0530

    Delete db/migrate directory

--- db/migrate/002_remove_number_from_risks.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNumberFromRisks < ActiveRecord::Migration
-  def self.up
-    remove_column :risks, :number
-  end
-
-  def self.down
-    add_column :risks, :number, :integer
-  end
-end
