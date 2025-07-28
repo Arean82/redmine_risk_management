06e4af3bba680a87a55dc50aef879a95dee8bb70
arean82 <38061300+Arean82@users.noreply.github.com>
2025-07-28 17:27:36 +0530

    Update project_patch.rb

--- lib/redmine_risk_management/patches/project_patch.rb
+++ lib/redmine_risk_management/patches/project_patch.rb
@@ -4,7 +4,7 @@ module RedmineRiskManagement
     module ProjectPatch
       def self.included(base) # :nodoc:
         base.class_eval do
-          unloadable # Send unloadable so it will not be unloaded in development
+         # unloadable # Send unloadable so it will not be unloaded in development
           has_many :risks, dependent: :destroy
         end
       end
